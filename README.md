51aa8eb2434683c17b3c6957ccd087ea70ae4017
popopo <yuliao.xu@rccchina.com>
2023-03-25 08:48:08 +0800

    ，，，

--- README.md
+++ README.md
@@ -1,9 +1,24 @@
 # scrapy_test
 scrapy_easy
+scrapy简单入门教学
 
 
+终端操作
+创建爬虫项目 
 scrapy startproject xxx
-cd easy_scrapy
+cd xxx
+创建爬虫文件 系统模板没啥用 可以自定义 
 scrapy genspider example example.com
 
+
 entrypoint.py中输入爬虫名字执行
+
+fy_spider为接口案例
+mzlggzy_spider为html案例
+教学案例没有数据库  作为简单入门教学 从理解如何使用scrapy抓取网页开始
+items middlewares pipelines settings stool各有功能
+items 用于定义抓取结果 需要存放的字段
+middleware 中间件可以用来设置例如请求重试，请求加代理等功能 
+pipelines 用作数据库传输
+stool 链接数据库
+settings  用于对爬虫任务做一些设置  例如 速率,执行目录,管道开关,中间件使用,日志存放等
---
+++ easy_scrapy/easy_scrapy/spiders/fy_spider.py
@@ -0,0 +1,82 @@
+import logging
+
+import scrapy
+from loguru import logger
+from scrapy.http import JsonRequest
+
+
+class FySpider(scrapy.Spider):
+    name = "fy_spider"
+    # 网页地址
+    site = "https://jyzx.fy.gov.cn/#/tradeInfo?collapseInd=1&currentGGInd=0&currentPInd=0&currentYm=1&num=1&publishTimeStart=&publishTimeEnd=",
+    post_url = "https://jyzx.fy.gov.cn/fyggfwpt-api-home-web/menhuJyxx/list"
+    post_data = {
+        "pageNo": 1,
+        "pageSize": 10,
+        "tableName": "ZC_WINBIDBULLETINFO",
+        "projectType": "Z2",
+        "publishTimeStart": "",
+        "areaCode": "",
+        "title": "",
+        "publishTimeEnd": "",
+        "mode": "",
+    }
+    total_page = 2
+
+    def start_requests(self):
+        """
+        循环获取每一个栏目
+        :return:
+        """
+        for page_no in range(1, self.total_page + 1):
+            post_url = self.post_url
+            post_data = self.post_data
+            post_data["pageNo"] = page_no
+            yield JsonRequest(
+                url=post_url,
+                callback=self.parse,
+                dont_filter=True,
+                data=post_data,
+            )
+
+    def parse(self, response, **kwargs):
+        """
+        获取list
+        :param response:
+        :param kwargs:
+        :return:
+        """
+        info_list = response.json()["data"].get("list", [])
+        for info in info_list:
+            guid = info["guid"]
+            info_url = f"https://jyzx.fy.gov.cn/fyggfwpt-api-home-web/menhuJyxx/detail?&guid={guid}"
+            info_title = info["title"]
+            origin_url = f"https://jyzx.fy.gov.cn/#/newTradeDetail?guid={guid}&nav=%E6%94%BF%E5%BA%9C%E9%87%87%E8%B4%AD&nav=%E4%B8%AD%E6%A0%87%E7%BB%93%E6%9E%9C%E5%85%AC%E5%91%8A&isNewSp=2"
+            yield JsonRequest(
+                url=info_url,
+                callback=self.parse_detail,
+                dont_filter=True,
+                cb_kwargs={
+                    'info_title': info_title,
+                    'origin_url': origin_url
+                }
+            )
+
+    def parse_detail(self, response, **kwargs):
+        """
+        获取详情页内容
+        :param response:
+        :param kwargs:
+        :return:
+        """
+        logger.info(kwargs['origin_url'])
+        news_content = response.json()["dataList"]["zbjg"][0]["winbidbulletincontent"]
+        content = "".join(news_content).strip()
+        item = {
+            'url': kwargs['origin_url'],
+            'title': kwargs['info_title'],
+            'content': content,  # 正文太长了就不写进去了 换成content就能看
+        }
+        logging.info(item)  # 结果放在default.log
+
+
